f157f5229f74405b2e13fa9b9acf2e47b2f4a13d
WijsenWesley <wesley.wijsen@ida-mediafoundry.be>
2022-06-16 09:37:42 +0200

    initial commit

---
+++ src/main/java/com/exercise/springbootsetup/SpringBootSetupApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.springbootsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSetupApplication.class, args);
+	}
+
+}
